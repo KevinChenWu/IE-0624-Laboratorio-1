96ca9e221405d057aef5bfde584ac3020aa2da10
KevinChenWu <hongwenchen.k.c.w.90@gmail.com>
2023-01-09 12:21:04 -0600

    Implementación preliminar de main, pendiente de ser ajustado

--- src/dado.c
+++ src/dado.c
@@ -3,6 +3,80 @@
 void lfsr16(unsigned short *rnd_number);
 void display_time(unsigned int time);
 
+void main(void) {
+	// Se pone GP0, GP1, GP2, GP3 y GP4 como salidas
+	// Se pone GP5 como entrada
+	// Se pone los demás pines como salidas
+	TRISIO = 0b00001000;
+
+	// Se pone las salidas en bajo
+	GPIO = 0b00000000;
+
+	unsigned int time = 1000;
+	unsigned short rnd_number = 1;
+	unsigned int dice_number = 0;
+
+	while (1)
+	{
+		lfsr16(&rnd_number);
+		if (GP3)
+		{
+			dice_number = 1 + (rnd_number % 6);
+			switch (dice_number)
+			{
+			case 1:
+				GP1 = ~GP1;
+				display_time(time);
+				GP1 = ~GP1;
+				break;
+			
+			case 2:
+				GP2 = ~GP2;
+				display_time(time);
+				GP2 = ~GP2;
+				break;
+			case 3:
+				GP1 = ~GP1;
+				GP2 = ~GP2;
+				display_time(time);
+				GP1 = ~GP1;
+				GP2 = ~GP2;
+				break;
+			case 4:
+				GP2 = ~GP2;
+				GP4 = ~GP4;
+				display_time(time);
+				GP2 = ~GP3;
+				GP4 = ~GP4;
+				break;
+			case 5:
+				GP1 = ~GP1;
+				GP2 = ~GP2;
+				GP4 = ~GP4;
+				display_time(time);
+				GP1 = ~GP1;
+				GP2 = ~GP3;
+				GP4 = ~GP4;
+				break;
+			case 6:
+				GP2 = ~GP2;
+				GP4 = ~GP4;
+				GP5 = ~GP5;
+				display_time(time);
+				GP2 = ~GP3;
+				GP4 = ~GP4;
+				GP5 = ~GP5;
+				break;
+			default:
+				break;
+			}
+		}
+		
+	}
+	
+
+}
+
 void lfsr16(unsigned short *rnd_number) {
 	if ((*rnd_number) & 1) {
     	(*rnd_number) >>= 1;
