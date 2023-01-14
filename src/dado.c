a1e89564f076021e8228295d65c8c36d0209c714
KevinChenWu <hongwenchen.k.c.w.90@gmail.com>
2023-01-13 23:32:17 -0600

    Corrección mínima de indentación

--- src/dado.c
+++ src/dado.c
@@ -85,10 +85,10 @@ void lfsr16(unsigned short *rnd_number) {
 	// Se pregunta si el último bit es 1
 	if ((*rnd_number) & 1) {
 		// Se desplaza el número un bit a la derecha
-    	(*rnd_number) >>= 1;
+		(*rnd_number) >>= 1;
 		// Se hace un XOR del número con la máscara y
 		// se actualiza a sí mismp
-    	(*rnd_number) ^= (1<<15) + (1<<14) + (1<<12) + (1<<3);
+		(*rnd_number) ^= (1<<15) + (1<<14) + (1<<12) + (1<<3);
 	}
 	else {
 		// Se desplaza el número un bit a la derecha
